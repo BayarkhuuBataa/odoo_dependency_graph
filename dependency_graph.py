8cd411925995e50ff94b18566f3b8bcb43de053b
Colin Wren <colin@gimpneek.com>
2015-07-25 20:03:11 +0100

    Rearranging module check and starting to add recursive function

--- dependency_graph.py
+++ dependency_graph.py
@@ -6,27 +6,55 @@ import erppeek
 class DependencyGraph:
 
 	@staticmethod
-	def get_erppeek_client(server='http://localhost:8069', db='openerp', user=None, password=None, verbose=False):
+	def get_erppeek_client(server='http://localhost:8069', db='openerp', user='admin', password='admin'):
+		"""
+		Get a ERPPeek client for us to use, if one not available then close the function
+		:param server: Server address (with XML-RPC port)
+		:param db: Name of database
+		:param user: Username to connect with
+		:param password: Password for username connecting with
+		:return: A erppeek.Client object we can use for XML-RPC calls
+		"""
 		client = None
 		try:
-			client = erppeek.Client(server, db=db, user=user, password=password, verbose=verbose)
+			client = erppeek.Client(server, db=db, user=user, password=password, verbose=False)
 		except:
 			print "Error connecting to {d} on {s} using credentials {u}:{p}".format(d=db, s=server, u=user, p=password)
 			print "Exiting...."
 			exit(1)
 		return client
 
-	def __init__(self, module, db='openerp', server='http://localhost:8069', user=None, password=None, verbose=False):
-		self.client = self.get_erppeek_client(server=server, db=db, user=user, password=password, verbose=verbose)
-		self.hierarchy = self.get_hierarchy_for_module(module)
-
-	def get_hierarchy_for_module(self, module):
-		mod_reg = 'ir.module.module'
-		dep_reg = 'ir.module.module.dependency'
-		# Check the module in question exists
-		module_present = self.client.search(mod_reg, [['name', '=', module]])
+	def __init__(self, module, db='openerp', server='http://localhost:8069', user='admin', password='admin'):
+		"""
+		Get a ERPPeek client, check the module we want to generate the graph for is in the database and generate the
+		hierarchy ready for formatting
+		:param module: Name of the module we want to generate the hierarchy for
+		:param db: Database to generate the hierarchy from
+		:param server: Odoo server address
+		:param user: Username - defaults to admin so we can access info on modules
+		:param password: Password for user we're connecting as
+		:return: An hierarchy object for the module specified
+		"""
+		self.client = self.get_erppeek_client(server=server, db=db, user=user, password=password)
+		self.mod_reg = 'ir.module.module'
+		self.dep_reg = 'ir.module.module.dependency'
+		# check that the module in question exists
+		module_present = self.client.search(self.mod_reg, [['name', '=', module]])
 		if not module_present:
 			print "{m} module not found in database".format(m=module)
 			exit(1)
-		# Generate the tree from the module
+		else:
+			self.hierarchy = self.get_hierarchy_for_module(module)
+
+	def get_hierarchy_for_module(self, module):
+		"""
+		Recursively get the hierarchy for a module
+		:param module: The module to get the hierarchy for
+		:return: Runs itself again with the new module or returns True
+		"""
+		# Search for modules that depend on the module
+		dependent_mod_ids = self.client.search(self.dep_reg, [['name', '=', module]])
+		# if dependent_mod_ids:
+		# 	dependent_mod_names = self.client.read(self.dep_reg, dependent_mod_ids, ['display_name'])
+		# 	print dependent_mod_names
 		return True
