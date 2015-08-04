e9a655da5699c72e096c87a6887e799b7db2423b
Colin Wren <colin@neovahealth.co.uk>
2015-08-04 21:44:00 +0100

    Starting to get the width based on number of nodes at depth implemented

--- svg_formatter.py
+++ svg_formatter.py
@@ -22,23 +22,21 @@ class SvgFormatter:
             # To get drawing area subtract both margins from height and width
             drawing_area = 100 - (margin * 2)
 
-            # To get the actual height take the padding times nodes (except the first)
-            # then take away the stroke (0.5 * 2) from this
-            actual_height = (drawing_area - (padding * (total_depth - 1))) - total_depth
-
             # To get rect height divide the available space between the nodes
-            rect_height = actual_height / total_depth
-            block_height = (rect_height + (padding * 2))
+            rect_height = (drawing_area - (padding * total_depth)) / total_depth
+            block_height = rect_height + (padding * 2)
             tree = self.graph
             for level in range(0, total_depth):
                 nodes_at_depth = [tree[node] for node in tree.expand_tree() if tree.depth(node) == level]
-                for node in nodes_at_depth:
-                    leaf_insert_y = (drawing_area - (level * block_height)) - total_depth
-                    leaf_insert_x = margin
+                rect_width = (drawing_area - (padding * len(nodes_at_depth))) / len(nodes_at_depth)
+                block_width = rect_width + padding
+                for index, node in enumerate(nodes_at_depth):
+                    leaf_insert_y = margin + ((total_depth - (level + 1)) * block_height)
+                    leaf_insert_x = margin + (index * block_width)
                     kwargs = {"class": 'level-{0}'.format(level)}
                     svg_document.add(svg_document.rect(insert = ('{0}%'.format(leaf_insert_x),
                                                                  '{0}%'.format(leaf_insert_y)),
-                                                       size = ('{0}%'.format(drawing_area),
+                                                       size = ('{0}%'.format(rect_width),
                                                                '{0}%'.format(rect_height)),
                                                        stroke_width = '0.5%',
                                                        stroke = 'black',
@@ -47,7 +45,7 @@ class SvgFormatter:
                                      )
 
                     text_offset_y = leaf_insert_y + (block_height/2)
-                    text_offset_x = drawing_area / 2
+                    text_offset_x = leaf_insert_x + (block_width/2)
                     svg_document.add(svg_document.text(node.tag,
                                                        insert = ('{0}%'.format(text_offset_x),
                                                                  '{0}%'.format(text_offset_y))
